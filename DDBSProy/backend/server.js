cc3109a6fd3fa3b504799a144fd3696e9ae8be6e
Erick González Esparza <50712522+erickgwav@users.noreply.github.com>
2024-06-08 17:53:11 -0600

    Todo preparao creo

---
+++ DDBSProy/backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'tu_usuario',
+  password: 'tu_contraseña',
+  database: 'tu_base_de_datos'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+app.get('/api/data', (req, res) => {
+  connection.query('SELECT * FROM tu_tabla', (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
